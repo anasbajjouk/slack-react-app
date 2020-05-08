7637064e797ca8c693218179603bf7c9949b69f4
anasbajjouk <a.bajjouk@cleo.one>
2020-05-08 15:21:08 +0200

    finalazing the app, pushed to production

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -1,22 +1,40 @@
-import React from 'react'
-import { Header, Segment, Input, Icon } from 'semantic-ui-react'
+import React from "react";
+import { Header, Segment, Input, Icon } from "semantic-ui-react";
 
 class MessagesHeader extends React.Component {
   render() {
+    const {
+      channelName,
+      numUniqueUsers,
+      handleSearchChange,
+      searchLoading,
+      isPrivateChannel,
+      handleStar,
+      isChannelStarred
+    } = this.props;
+
     return (
       <Segment clearing>
         {/* Channel Title */}
         <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
           <span>
-            Channel
-            <Icon name={'star outline'} color="black" />
+            {channelName}
+            {!isPrivateChannel && (
+              <Icon
+                onClick={handleStar}
+                name={isChannelStarred ? "star" : "star outline"}
+                color={isChannelStarred ? "yellow" : "black"}
+              />
+            )}
           </span>
-          <Header.Subheader>2 Users</Header.Subheader>
+          <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
 
         {/* Channel Search Input */}
         <Header floated="right">
           <Input
+            loading={searchLoading}
+            onChange={handleSearchChange}
             size="mini"
             icon="search"
             name="searchTerm"
@@ -24,8 +42,8 @@ class MessagesHeader extends React.Component {
           />
         </Header>
       </Segment>
-    )
+    );
   }
 }
 
-export default MessagesHeader
+export default MessagesHeader;
